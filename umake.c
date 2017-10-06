559c6b01ad4a9652b36e0afc03535e2af225d686
maiv <maiv@wwu.edu>
2017-10-05 20:47:30 -0700

    added helper function countArg, debugging I think I am having a problem with how I am checking for null pointers

--- umake.c
+++ umake.c
@@ -1,163 +1,1 @@
-/* CSCI 347 micro-make
- * 
- * 09 AUG 2017, Aran Clauson
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-/* CONSTANTS */
-/* PROTOTYPES */
-/* Parse Arguments
- * returns an array of pointers that point to characters in line
- */
-char** arg_parse(char* line);
-/* Process Line
- * line   The command line to execute.
- * This function interprets line as a command line.  It creates a new child
- * process to execute the line and waits for that process to complete. 
- */
-void processline(char* line);
-/* Main entry point.
- * argc    A count of command-line arguments 
- * argv    The command-line argument valus
- *
- * Micro-make (umake) reads from the uMakefile in the current working
- * directory.  The file is read one line at a time.  Lines with a leading tab
- * character ('\t') are interpreted as a command and passed to processline minus
- * the leading tab.
- */
-int main(int argc, const char* argv[]) {
-  FILE* makefile = fopen("./uMakefile", "r");
-  size_t  bufsize = 0;
-  char*   line    = NULL;
-  ssize_t linelen = getline(&line, &bufsize, makefile);
- 
-  while(-1 != linelen) {
-    if(line[linelen-1]=='\n') {
-      linelen -= 1;
-      line[linelen] = '\0';
-    }
-    if(line[0] == '\t') 
-      processline(&line[1]);
-    linelen = getline(&line, &bufsize, makefile);
-  }
-  free(line);
-  return EXIT_SUCCESS;
-}
-
-/* Parse Argument
- * Should return an array of pointers
- */
-char** arg_parse(char* line) {
-	//Storing starting position	
-	int aCounter = 0;
-        int argFound = 0;
-	char* start;
-	start = line;
-	//Iterate through line and find number of arguments
-	while(*line != '\0'){
-            //have 2 flags, 
-            //one that detects when a char is found, iterate until you find a whitespace		
-            if(*line != ' ' && argFound == 0){
-                //char found
-                argFound++;
-                if(argFound == 1){
-                    aCounter++; //arg found
-                }
-                printf("Arg found, pointer: %s \n", line);
-                line++;
-            }else{	
-                //reset argfound
-                argFound = 0;
-                if(*line == '\0'){
-                    //if pointer already at null dont increment
-                    
-                }else{
-                    //skipping extra characters
-                    printf("Skip, pointer: %s \n", line);
-                    line++;
-                }
-            }
-	}
-        
-	//reset pointer position 
-	line = start;	
-	//at this point you have your number of arguments
-
-	//malloc your number of arguments +1
-	aCounter++;
-        printf("%d", aCounter);
-        char** arguments = malloc(aCounter);
-	char* currArg;
-	int argsInserted;
-	//Array is now created using malloc
-	
-	//Iterate through line again and store pointer location of first char of argument.
-	//Store in char** array
-	//Iterate until you find a whitespace or null pointer. Repeat
-	while(*line != '\0'){
-		//first character is not null, arg is not null
-		if(*line == ' '){
-			//Whitespace skip
-			*line++;	
-		}else{
-			//Something other than a whitespace found
-			//Store pointer in array
-			currArg = line;
-			arguments[argsInserted] = currArg;
-			argsInserted++;
-			
-			//Once you have stored the pointer location of the first char, add in a null 				pointer in the first white space after that argument
-			//Assume that the rest of the characters until the next whitespace are part 				of the argument. for now continue interating until next whitespace is found
-			while(*line != ' '){
-				line++;
-			}
-			//at this point should be at first new whitespace that is found
-			if(*line == ' '){
-				//first whitespace after arg found, replace with null pointer
-				*line = '\0';				
-			}
-			line++;
-		} 
-	}
-	//one extra null pointer at the end is required
-	arguments[argsInserted] = '\0';
-}
- 
-/* Process Line
- * 
- */
-void processline (char* line) {
-  
-  //modify processline to use arg_parse
-  printf("Calling arg_parse \n");
-  char** test;
-  test = arg_parse(line);
-
-  const pid_t cpid = fork();
-  switch(cpid) {
-  case -1: {
-    perror("fork");
-    break;
-  }
-  case 0: {
-    execlp(line, line, (char*)(0));
-    perror("execlp");
-    exit(EXIT_FAILURE);
-    break;
-  }
-  default: {
-    int   status;
-    const pid_t pid = wait(&status);
-    if(-1 == pid) {
-      perror("wait");
-    }
-    else if (pid != cpid) {
-      fprintf(stderr, "wait: expected process %d, but waited for process %d",
-              cpid, pid);
-    }
-    break;
-  }
-  }
-}
+/* CSCI 347 micro-make *  * 09 AUG 2017, Aran Clauson */#include <stdio.h>#include <stdlib.h>#include <unistd.h>#include <sys/wait.h>/* CONSTANTS *//* PROTOTYPES *//* Parse Arguments * returns an array of pointers that point to characters in line */int countArg(char* line); //helper function that counts argschar** arg_parse(char* line);/* Process Line * line   The command line to execute. * This function interprets line as a command line.  It creates a new child * process to execute the line and waits for that process to complete.  */void processline(char* line);/* Main entry point. * argc    A count of command-line arguments  * argv    The command-line argument valus * * Micro-make (umake) reads from the uMakefile in the current working * directory.  The file is read one line at a time.  Lines with a leading tab * character ('\t') are interpreted as a command and passed to processline minus * the leading tab. */int main(int argc, const char* argv[]) {  FILE* makefile = fopen("./uMakefile", "r");  size_t  bufsize = 0;  char*   line    = NULL;  ssize_t linelen = getline(&line, &bufsize, makefile);   while(-1 != linelen) {    if(line[linelen-1]=='\n') {      linelen -= 1;      line[linelen] = '\0';    }    if(line[0] == '\t')       processline(&line[1]);    linelen = getline(&line, &bufsize, makefile);  }  free(line);  return EXIT_SUCCESS;}/* Helper function for arg_parse * Counts number of arguments */int countArg(char* line){	//Storing starting position		int aCounter = 0;        int argFound = 0;        printf("Arg found, pointer: %s \n", line);	//Iterate through line and find number of arguments	while(*line != '\0'){            //have 2 flags,             //one that detects when a char is found, iterate until you find a whitespace		            if(*line != ' ' && argFound == 0){                //char found                argFound++;                if(argFound == 1){                    aCounter++; //arg found                }                printf("Arg found, pointer: %s \n", line);                printf("argFound: %d \n", argFound);                line++;            }else{                if(*line != '\0' && *line != ' '){                    //if pointer already at null dont increment                    //skipping extra characters                    printf("Skip, pointer: %s \n", line);                    argFound++;                    line++; //test input for longer arguments                    dprintf(2, "incremented \n");                 }else if (*line == ' '){                    //reset argfound when whitespace is found                    argFound = 0;                    line++;                }            }	}	printf("Number of args successfully counted \n");        printf("Pointer in countArg: %s \n", line);        return aCounter;}/* Parse Argument * Should return an array of pointers */char** arg_parse(char* line) {        //copying pointer to count args        int aCounter;        char* lineDup = line;        printf("Counting number of args \n");        aCounter = countArg(lineDup);        printf("Pointer in arg_parse: %s \n", line);        printf("Number of args: %d \n", aCounter);        aCounter++;        printf("Total space for args: %d \n", aCounter);        char** arguments = (char**) malloc(aCounter*sizeof(char*)); //seg faults at this line        arguments[aCounter-1] = 0L;        dprintf(2, "Point 1 \n");         	char* currArg;	int argsInserted = 0;	//Array is now created using malloc        	//Iterate through line again and store pointer location of first char of argument.	//Store in char** array	//Iterate until you find a whitespace or null pointer. Repeat	while(*line != '\0'){		//first character is not null, arg is not null		if(*line == ' '){			//Whitespace skip			line++;			}else{			//Something other than a whitespace found			//Store pointer in array			currArg = line;			arguments[argsInserted] = currArg;			argsInserted++;			dprintf(2, "%s \n", line); 			//Once you have stored the pointer location of the first char, add in a null 				pointer in the first white space after that argument			//Assume that the rest of the characters until the next whitespace are part 				of the argument. for now continue interating until next whitespace is found			while(*line != ' '){				line++;			}			//at this point should be at first new whitespace that is found			if(*line == ' '){                            //first whitespace after arg found, replace with null pointer                            line = '\0';							}else if (*line == '\0'){                            //check for when it reaches the end of line and find a null pointer                            break;                        }			line++;		} 	}	//one extra null pointer at the end is required	//arguments[argsInserted] = '\0';	        return arguments;} /* Process Line *  */void processline (char* line) {    //modify processline to use arg_parse  printf("Calling arg_parse \n");  char** test;  test = arg_parse(line);  const pid_t cpid = fork();  switch(cpid) {  case -1: {    perror("fork");    break;  }  case 0: {    execlp(line, line, (char*)(0));    perror("execlp");    exit(EXIT_FAILURE);    break;  }  default: {    int   status;    const pid_t pid = wait(&status);    if(-1 == pid) {      perror("wait");    }    else if (pid != cpid) {      fprintf(stderr, "wait: expected process %d, but waited for process %d",              cpid, pid);    }    break;  }  }}
