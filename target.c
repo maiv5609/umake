1d16f44ff82a0b78b961ea042f2131acb12a7c28
maiv <maiv@wwu.edu>
2017-10-19 13:59:33 -0700

    finished making structure of targetlist, no testing done yet

--- target.c
+++ target.c
@@ -12,25 +12,6 @@ https://stackoverflow.com/questions/4983010/invalid-type-argument-of-c-structs
 */
 
 
-/* Structure that will hold the target's name
- * an array of the target's dependencies, and the target's rules
- */
-typedef struct targetList{
-	char* currTarget; //char* or char of target's name
-	char** dependencies;	//array of target's dependencies
-	char** rules;	//array of target's rules
-	struct targetList* next;	//pointer to node of next target (or null if last child)
-} targetList;
-
-/* PROTOTYPES */
-void* findTarget(char* name);
-void* findTargetnode(char* name, targetList list);
-void addNode(char* name, char** dependencies, char** rules);
-
-//tList is the global list of targets that I am adjusting
-targetList tList;
-
-
 /* function to get find indicated target
  * will return null if list is empty or not found in list
  * adjusts pointer to whichever node's name matches the one I am searching for
@@ -41,10 +22,10 @@ void* findTarget(char* name){
 /* Helper function for findTarget
  *
  */
-void* findTargetnode(char* name, targetList list){
-	if(*list->currTarget == NULL){
+void* findTargetnode(char* name, targetList* list){
+	if(list->currTarget == NULL){
 		return NULL;
-	}else if (strcmp(*list->currTarget, name) == 0){
+	}else if (strcmp(list->currTarget, name) == 0){
 		 //target found return contents
 		 return list -> rules;
 	 }else{
@@ -57,14 +38,14 @@ void* findTargetnode(char* name, targetList list){
   * Use this to build list
   */
 void addNode(char* name, char** dependencies, char** rules){
-	targetList n = malloc(sizeof(targetList));
+	targetList* n = malloc(sizeof(targetList));
 	if(!n){
 		perror("malloc");
 		exit(1);
 	}
 	//assignment of variables
 	//apprantly Strdupa does the same thing but free memory automatically when you exit the funtion where it was called.
-	n -> currTarget = Strdup(name); //I might need to free memory here
+	n -> currTarget = strdup(name); //I might need to free memory here
 	n -> dependencies = dependencies;
 	n -> next = tList;
 	tList = n;
--- target.h
+++ target.h
@@ -1,5 +1,21 @@
+/* Structure that will hold the target's name
+ * an array of the target's dependencies, and the target's rules
+ */
+typedef struct targetList{
+	char* currTarget; //char* or char of target's name
+	char** dependencies;	//array of target's dependencies
+	char** rules;	//array of target's rules
+	struct targetList *next;	//pointer to node of next target (or null if last child)
+} targetList;
+
+//tList is the global list of targets that I am adjusting
+targetList* tList;
+
 /* function to read current target's rules
  * accesses current target's rule and returns the rule as a line (char*?)
  * will probably have to loop through until we hit a null pointer on the array
  */
 char* getRules(char* currRule);
+void* findTarget(char* name);
+void* findTargetnode(char* name, targetList* list);
+void addNode(char* name, char** dependencies, char** rules);
