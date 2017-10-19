409f39e376a4ab3519c9373a9093dd930b45b8f7
maiv <maiv@wwu.edu>
2017-10-18 19:19:53 -0700

    worked more on setting up linked list structure

--- target.c
+++ target.c
@@ -1,44 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 #include "target.h"
+
+/*
+page about invalid type argument '->' error
+I am probably declaring someone incorrectly
+https://stackoverflow.com/questions/4983010/invalid-type-argument-of-c-structs
+*/
 
 
 /* Structure that will hold the target's name
  * an array of the target's dependencies, and the target's rules
  */
-typedef struct target{
-	//char* or char of target's name
-	char* currNode;
+typedef struct targetList{
+	char* currTarget; //char* or char of target's name
+	char** dependencies;	//array of target's dependencies
+	char** rules;	//array of target's rules
+	struct targetList* next;	//pointer to node of next target (or null if last child)
+} targetList;
 
-	//array of target's dependencies
-	char** dependencies;
+/* PROTOTYPES */
+void* findTarget(char* name);
+void* findTargetnode(char* name, targetList list);
+void addNode(char* name, char** dependencies, char** rules);
 
-	//array of target's rules
-	char** rules;
+//tList is the global list of targets that I am adjusting
+targetList tList;
 
-	//pointer to node of next target (or null if last child)
-	char* nextNode;
-} target;
 
-/* Function to create new target which would start the linked list
+/* function to get find indicated target
+ * will return null if list is empty or not found in list
+ * adjusts pointer to whichever node's name matches the one I am searching for
+ */
+void* findTarget(char* name){
+	return findTargetnode(name, tList);
+}
+/* Helper function for findTarget
  *
  */
+void* findTargetnode(char* name, targetList list){
+	if(*list->currTarget == NULL){
+		return NULL;
+	}else if (strcmp(*list->currTarget, name) == 0){
+		 //target found return contents
+		 return list -> rules;
+	 }else{
+		 //not found, return next node
+		 return findTargetnode(name, list->next);
+	 }
+ }
 
-
-/* Function to add target to next
-*
-*/
-
-
-/* Function to read current target's rules
- * accesses current target's rule and returns the rule as a line (char*?)
- * will probably have to loop through until we hit a null pointer on the array
- */
-char* getRules(char* currRule){
-
-}
-
-/* function to get next target in linked list
- * will return null if list is empty or if current target is last one
- */
-char* nextTarget(){
-
+ /* Function used to add a target to the list
+  * Use this to build list
+  */
+void addNode(char* name, char** dependencies, char** rules){
+	targetList n = malloc(sizeof(targetList));
+	if(!n){
+		perror("malloc");
+		exit(1);
+	}
+	//assignment of variables
+	//apprantly Strdupa does the same thing but free memory automatically when you exit the funtion where it was called.
+	n -> currTarget = Strdup(name); //I might need to free memory here
+	n -> dependencies = dependencies;
+	n -> next = tList;
+	tList = n;
 }
--- target.h
+++ target.h
@@ -3,8 +3,3 @@
  * will probably have to loop through until we hit a null pointer on the array
  */
 char* getRules(char* currRule);
-
-/* function to get next target in linked list
- * will return null if list is empty or if current target is last one
- */
-char* nextTarget()
