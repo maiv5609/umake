ae12f2568b319954c4ef8e6045d22364e357f891
maiv <maiv@wwu.edu>
2017-10-20 21:59:41 -0700

    setting up structure for rules linked list and being able to parse from it, currently super messy from experimenting

--- target.c
+++ target.c
@@ -13,6 +13,8 @@
 void* findTarget(char* name){
 	return findTargetnode(name, tList);
 }
+
+
 /* Helper function for findTarget
  *
  */
@@ -21,12 +23,19 @@ void* findTargetnode(char* name, targetList* list){
 		return NULL;
 	}else if (strcmp(list->currTarget, name) == 0){
 		 //target found return contents
+		 ruleList* current = &list;
+
+		 //char* currentRule = current->currRule;
 
 		 /*
 		 dprintf(2, "Name: %s \n", name);
 		 dprintf(2, "Dependencies: %s \n", list->dependencies);
-		 dprintf(2, "Rules: %s \n \n", list->rules);
+		 dprintf(2, "Rules: %s \n \n", rList->currRule);
 		 */
+		 //https://stackoverflow.com/questions/23178741/access-members-of-a-structure-from-void-pointer-inside-a-structure
+		 //This page might detail how to handle looking at a value inside a nested struct
+
+		 dprintf(2, "Rules: %s \n \n", ((struct ruleList *)current.rules->currRule));
 		 return list->rules;
 	 }else{
 		 //not found, return next node
@@ -37,17 +46,29 @@ void* findTargetnode(char* name, targetList* list){
  /* Function used to add a target to the list
   * creates new node and makes this new node the head of the previous list
   */
-void addNode(char* name, char* dependencies, char* rules){
+void addNode(char* name, char* dependencies, ruleList* rules){
 	targetList* n = malloc(sizeof(targetList));
 	if(!n){
 		perror("malloc");
 		exit(1);
 	}
 	//assignment of variables
-	//apprantly Strdupa does the same thing but free memory automatically when you exit the funtion where it was called.
-	n->currTarget = strdup(name); //I might need to free memory here
+	n->currTarget = strdup(name); //I need to free memory here
 	n->dependencies = dependencies;
 	n->rules = rules;
 	n->next = tList; //adds rest of list to tail
 	tList = n;
+}
+
+void addRule (char* line){
+	ruleList* list = malloc(sizeof(ruleList));
+	if(!list){
+		perror("malloc");
+		exit(1);
+	}
+	dprintf(2, "Curr Rule: %s \n", line);
+	list->currRule = strdup(line); //seg faults
+	list->next = rList;
+	rList = list;
+
 }
--- target.h
+++ target.h
@@ -4,12 +4,26 @@
 typedef struct targetList{
 	char* currTarget; //char* or char of target's name
 	char* dependencies;	//line of target's dependencies
-	char* rules;	//target's rules
+	struct ruleList* rules;	//target's rules
 	struct targetList* next;	//pointer to node of next target (or null if last child)
 } targetList;
 
+/* Structure will hold rules in a linked list
+ *
+ */
+typedef struct ruleList{
+	char* currRule;
+	struct ruleList* next;
+}ruleList;
+
 //tList is the global list of targets that I am adjusting
 targetList* tList;
+
+//collects rules for current target
+ruleList* rList;
+
+
+void addRule (char* line);
 
 /* function to read current target's rules
  * accesses current target's rule and returns the rule as a line (char*?)
@@ -18,4 +32,4 @@ targetList* tList;
 char* getRules(char* currRule);
 void* findTarget(char* name);
 void* findTargetnode(char* name, targetList* list);
-void addNode(char* name, char* dependencies, char* rules);
+void addNode(char* name, char* dependencies, ruleList* rules);
